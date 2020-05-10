10036c59fd51596c792ab7f6ee0d937fc6a96941
Harshil <harshilame@gmail.com>
2020-05-10 13:00:23 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Animated, View, Image, TouchableOpacity, Easing, 
+} from 'react-native';
+
+
+const App = () => {
+
+  const [val, setVal] = React.useState(0);
+  
+const spinValue =  new Animated.Value(val);
+  rotateImage = () => {
+    setVal(val + 1);
+  };
+
+  spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '90deg']
+  });
+  
+  return (
+
+    <>
+    <View>
+    <Animated.View>
+              <Animated.Image
+        style={{transform: [{rotate: spin}], height: 400, width: 400,  alignSelf: 'center', marginTop: 80}}
+        source={require('./src/assets/harshil.jpeg')} />
+        </Animated.View>
+        <TouchableOpacity style={{paddingRight: 4, marginTop: 100}} onPress={this.rotateImage}>
+                <Image style={{width: 60, height: 60,  alignSelf: 'center'}}
+                source={require('./src/assets/rotate.png')}/>
+                </TouchableOpacity>
+    </View>
+     
+    </>
+  );
+};
+
+
+export default App;
